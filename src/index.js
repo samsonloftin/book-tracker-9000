ab093b079d489ddb3b97c105af27fffb3f5d7bc6
Samson <47623975+samsonloftin@users.noreply.github.com>
2019-07-03 10:45:06 -0700

    Fix Index
    Changing the index to point to the BooksApp class rather than the non-existent  App class

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import BooksApp from './App';
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <BooksApp />
   </BrowserRouter>, document.getElementById('root'));
