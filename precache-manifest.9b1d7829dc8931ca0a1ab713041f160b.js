2078fe9d598397758c1d5bd879341b32deb0ba28
Samson <47623975+samsonloftin@users.noreply.github.com>
2019-07-03 11:53:44 -0700

    Updates

---
+++ precache-manifest.9b1d7829dc8931ca0a1ab713041f160b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "227205cede2e18b79abe",
+    "url": "/samsonloftin/book-tracker-9000/static/css/main.04d37b3f.chunk.css"
+  },
+  {
+    "revision": "227205cede2e18b79abe",
+    "url": "/samsonloftin/book-tracker-9000/static/js/main.227205ce.chunk.js"
+  },
+  {
+    "revision": "ed2fa78c83565fa2d1ef",
+    "url": "/samsonloftin/book-tracker-9000/static/js/1.ed2fa78c.chunk.js"
+  },
+  {
+    "revision": "3089d42d948769f1f687",
+    "url": "/samsonloftin/book-tracker-9000/static/js/runtime~main.3089d42d.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/samsonloftin/book-tracker-9000/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/samsonloftin/book-tracker-9000/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/samsonloftin/book-tracker-9000/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9699041479e4c66b9b9321bda1adbbe3",
+    "url": "/samsonloftin/book-tracker-9000/index.html"
+  }
+];
