bd144b2aa2fd49dd7cc9c5b170cfa71970b1ac45
Samson <47623975+samsonloftin@users.noreply.github.com>
2019-09-30 04:03:56 -0700

    Updates

---
+++ precache-manifest.8dc303ba0e951c7c7457eefc6e36bab7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "516a973107960a913879679c21c6d6b9",
+    "url": "/book-tracker-9000/index.html"
+  },
+  {
+    "revision": "f8879d9ff35c5380155b",
+    "url": "/book-tracker-9000/static/css/main.dfe9f5aa.chunk.css"
+  },
+  {
+    "revision": "45815012a76402cc3bf0",
+    "url": "/book-tracker-9000/static/js/2.bef0a43b.chunk.js"
+  },
+  {
+    "revision": "f8879d9ff35c5380155b",
+    "url": "/book-tracker-9000/static/js/main.e628e6e3.chunk.js"
+  },
+  {
+    "revision": "db3e4bbb663b5dca5655",
+    "url": "/book-tracker-9000/static/js/runtime~main.6b17a528.js"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/book-tracker-9000/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/book-tracker-9000/static/media/arrow-drop-down.9d4e3f36.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/book-tracker-9000/precache-manifest.efa1e6e7d36b7a08705cabd811facea8.js"
+  "/book-tracker-9000/precache-manifest.8dc303ba0e951c7c7457eefc6e36bab7.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/book-tracker-9000/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/book-tracker-9000/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
